0efc331afb78ee22b9bb0352f49dcbb9e5fd1724
masha <mashavityuk2@gmail.com>
2017-08-06 23:45:49 +0300

    init

---
+++ assets/js/common.js
@@ -0,0 +1,33 @@
+﻿$( document ).ready(function () {
+
+
+
+
+
+
+    particlesJS.load('animated-bg', '/assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+    //
+    //
+    //
+    // $('body').on('mousemove', function(e){
+    //     eyeTrack(e);
+    // });
+    // function eyeTrack(e) {
+    //     e = e || window.event;
+    //     var
+    //         delta = (window.innerHeight / 2 - $('.eye-block .eye-wr .eye').offset().top) / 2,
+    //         xPos = (e.clientX * 100) / window.innerWidth,
+    //         yPos = ((e.clientY + delta) * 100) / window.innerHeight;
+    //
+    //     yPos > 90 ? yPos = 90 : yPos;
+    //     xPos > 90 ? xPos = 90 : xPos;
+    //
+    //
+    //     $('.eye-block .eye-wr .eye .eye-pupil').css({
+    //         top: yPos + '%',
+    //         left: xPos + '%'
+    //     })
+    // }
+})
